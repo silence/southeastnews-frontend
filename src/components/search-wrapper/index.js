c1d1f08aa0550079371f4e2b6287482dd0d4b8ba
silence <silence.since2017@gmail.com>
2018-10-19 18:04:44 +0800

    feat: beautify search page

--- src/components/search-wrapper/index.js
+++ src/components/search-wrapper/index.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
-import { Layout, Menu, Icon } from 'antd'
+import { Layout, Menu, Icon, Input, Card, Form, Checkbox, DatePicker, Divider } from 'antd'
 import styles from './index.module.scss'
+import TagSelect from '../TagSelect'
 
+const CheckboxGroup = Checkbox.Group
+const { RangePicker } = DatePicker
+const FormItem = Form.Item
 const { Header, Content, Footer } = Layout
+const Search = Input.Search
+const websitesFromServer = ['metrotv', 'sindonews', 'liputan6', 'ripublika']
+const lanOptionsFromServer = [
+    { label: '越南语', value: 'Vietnam' },
+    { label: '印度尼西亚语', value: 'Lndonesia', disabled: true },
+    { label: '马来西亚语', value: 'Malaysia', disabled: true }
+]
 
 class SearchWrapper extends Component {
-    handleClick = e => {
+    state = {
+        selectedWebsites: []
+    }
+
+    handleWebsiteCheck = (website, isCheck) => {}
+
+    handleNavClick = e => {
         console.log(e)
         e.domEvent.preventDefault()
         if (e.key === 'admin') {
@@ -24,18 +41,32 @@ class SearchWrapper extends Component {
                 <Icon type="user" theme="outlined" /> 管理员
             </Menu.Item>
         ) : null
+        const { getFieldDecorator } = this.props.form
+        const formLayout = {
+            labelCol: { span: 2 },
+            wrapperCol: { span: 14, offset: 1 }
+        }
         return (
             <Layout className={styles.layout}>
-                <Header style={{ background: '#f0f2f5', padding: '0' }}>
+                <Header
+                    style={{
+                        padding: '0'
+                    }}
+                >
                     <div className={styles['nav-header']}>
                         <Menu
-                            onClick={this.handleClick}
+                            onClick={this.handleNavClick}
                             mode="horizontal"
-                            style={{ lineHeight: '64px' }}
+                            style={{ lineHeight: '63px' }}
                         >
                             <Menu.Item
                                 key="displayLogo"
-                                style={{ 'border-bottom': '0', fontSize: 20, color: '#1890ff' }}
+                                style={{
+                                    borderBottom: '0',
+                                    fontSize: 20,
+                                    color: '#1890ff',
+                                    padding: '0'
+                                }}
                             >
                                 东南亚新闻搜索
                             </Menu.Item>
@@ -46,8 +77,54 @@ class SearchWrapper extends Component {
                         </Menu>
                     </div>
                 </Header>
-                <Content style={{ padding: '0 50px' }}>
-                    <h1>content</h1>
+                <Content>
+                    <div className={styles['page-content']}>
+                        <div className={styles['content-wrapper']}>
+                            <h1>搜索列表</h1>
+                            <div className={styles['search-wrapper']}>
+                                <Search
+                                    placeholder="关键字、作者、新闻标题"
+                                    enterButton="搜索"
+                                    size="large"
+                                    onSearch={value => console.log(value)}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles['list-content']}>
+                        <div className={styles['grid-content']}>
+                            <Card bordered={false}>
+                                <Form layout="horizontal">
+                                    <FormItem label="网站选择" {...formLayout}>
+                                        {getFieldDecorator('website', {})(
+                                            <TagSelect>
+                                                {websitesFromServer.map(website => {
+                                                    return (
+                                                        <TagSelect.Option
+                                                            value={website}
+                                                            key={website}
+                                                        >
+                                                            {website}
+                                                        </TagSelect.Option>
+                                                    )
+                                                })}
+                                            </TagSelect>
+                                        )}
+                                    </FormItem>
+                                    <Divider dashed />
+                                    <FormItem label="语种选择" {...formLayout}>
+                                        {getFieldDecorator('lanSelect', {
+                                            initialValue: ['Vietnam']
+                                        })(<CheckboxGroup options={lanOptionsFromServer} />)}
+                                    </FormItem>
+                                    <Divider dashed />
+                                    <FormItem label="时间范围选择" {...formLayout}>
+                                        {getFieldDecorator('range-picker')(<RangePicker />)}
+                                    </FormItem>
+                                </Form>
+                            </Card>
+                        </div>
+                    </div>
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>©2018 StarStudio</Footer>
             </Layout>
@@ -55,4 +132,8 @@ class SearchWrapper extends Component {
     }
 }
 
-export default SearchWrapper
+export default Form.create({
+    onValuesChange(_, values) {
+        console.log(values)
+    }
+})(SearchWrapper)
