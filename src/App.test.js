6997e60582d8f0f8f380df78e57eafe97e0e6e01
silence <silence.since2017@gmail.com>
2018-12-03 23:26:13 +0800

    fix: fix npm run test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import App from './App'
+import { LocaleProvider } from 'antd'
+import zh_CN from 'antd/lib/locale-provider/zh_CN'
+import 'moment/locale/zh-cn'
+import './main.scss'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+    const div = document.createElement('div')
+    ReactDOM.render(
+        <BrowserRouter>
+            <Provider store={store}>
+                <LocaleProvider locale={zh_CN}>
+                    <App />
+                </LocaleProvider>
+            </Provider>
+        </BrowserRouter>,
+        div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+})
