8a91d766316c1486ab40d10e66007f366d7cd4fa
silence <silence.since2017@gmail.com>
2018-12-11 11:29:48 +0800

    wtf: add production build to origin

---
+++ build/precache-manifest.0ad3f754d201815e2ee71d53117c9dac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "70981b49ed2556603a1ba91acd7dd1c5",
+    "url": "./static/media/login.70981b49.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "dba618083a90f329843c",
+    "url": "./static/js/main.dba61808.chunk.js"
+  },
+  {
+    "revision": "4c4ebd1886a33e38097b",
+    "url": "./static/js/1.4c4ebd18.chunk.js"
+  },
+  {
+    "revision": "dba618083a90f329843c",
+    "url": "./static/css/main.0d07d388.chunk.css"
+  },
+  {
+    "revision": "4c4ebd1886a33e38097b",
+    "url": "./static/css/1.6cd43039.chunk.css"
+  },
+  {
+    "revision": "de0d96bb28e5075aa5e91026c4639e92",
+    "url": "./index.html"
+  }
+];
